c53fd4b887fea39dd0a840863030f9110644897f
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-01-08 11:08:19 +0530

    play after go fullscreen

--- webrtc/gui/hid.ts
+++ webrtc/gui/hid.ts
@@ -91,6 +91,7 @@ export class HID {
         this.shortcuts = new Array<Shortcut>();
         this.shortcuts.push(new Shortcut(ShortcutCode.Fullscreen,[KeyCode.Ctrl,KeyCode.Shift,KeyCode.F],(()=> {
             this.video.parentElement.requestFullscreen();
+            this.video.play();
         })))
         this.shortcuts.push(new Shortcut(ShortcutCode.PointerLock,[KeyCode.Ctrl,KeyCode.Shift,KeyCode.P],(()=> {
             if(!document.pointerLockElement) {
