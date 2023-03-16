01b943c648022e2a269fb8271f3938014231c61d
carosaome <sieunhankiet@gmail.com>
2023-03-16 16:04:18 +0700

    add vadilator to sign-up form

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -22,150 +22,150 @@ import { redirect } from "next/navigation";
 import { useRouter } from "next/navigation";
 
 export function Copyright(props) {
-    return (
-        <Typography
-            variant="body2"
-            color="text.secondary"
-            align="center"
-            {...props}
-        >
-            {"Copyright © "}
-            <Link color="inherit" href="https://mui.com/">
-                ThinkMay
-            </Link>{" "}
-            {new Date().getFullYear()}
-            {"."}
-        </Typography>
-    );
+	return (
+		<Typography
+			variant="body2"
+			color="text.secondary"
+			align="center"
+			{...props}
+		>
+			{"Copyright © "}
+			<Link color="inherit" href="https://mui.com/">
+				ThinkMay
+			</Link>{" "}
+			{new Date().getFullYear()}
+			{"."}
+		</Typography>
+	);
 }
 
 const theme = createTheme();
 
 export default function SignIn() {
-    const { isUserAuthenticated, user } = useAuth();
-    const router = useRouter();
+	const { isUserAuthenticated, user } = useAuth();
+	const router = useRouter();
 
-    React.useEffect(() => {
-        if (isUserAuthenticated()) {
-            // router.push('/')
-            // router.replace(`/`);
-            console.log("chang roue");
-        }
-    }, [user]);
+	React.useEffect(() => {
+		if (isUserAuthenticated()) {
+			// router.push('/')
+			// router.replace(`/`);
+			console.log("chang roue");
+		}
+	}, [user]);
 
-    const clientSupabase = new VirtualOSBrowserCore();
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const dataForm = new FormData(event.currentTarget);
-        const { data, error } = await supabase.auth.signUp({
-            email: dataForm.get("email"),
-            password: dataForm.get("password"),
-        });
-        console.log(data);
-    };
-    return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <Box
-                        component="form"
-                        onSubmit={handleSubmit}
-                        noValidate
-                        sx={{ mt: 1 }}
-                    >
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <FormControlLabel
-                            control={
-                                <Checkbox value="remember" color="primary" />
-                            }
-                            label="Remember me"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            onClick={() => {
-                                // router.push("/");
-                            }}
-                        >
-                            Sign In
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                {/* <Link   */}
-                                <NextLink href={AppRoutes.SIGN_UP}>
-                                    {"Don't have an account? Sign Up"}
-                                </NextLink>
-                                {/* </Link> */}
-                            </Grid>
-                        </Grid>
-                        <Stack></Stack>
-                        <Grid
-                            container
-                            justifyContent={"center"}
-                            alignItems="center"
-                        >
-                            <Grid item xs="auto">
-                                <Typography variant="h5">
-                                    Or sign in with:
-                                </Typography>{" "}
-                            </Grid>
-                            <Grid item xs={"auto"}>
-                                <IconButton
-                                    onClick={async () => {
-                                        await clientSupabase.LoginWithGoogle();
-                                    }}
-                                >
-                                    <GoogleIcon
-                                        sx={{ fontSize: 30 }}
-                                        color="primary"
-                                    />
-                                </IconButton>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-                <Copyright sx={{ mt: 8, mb: 4 }} />
-            </Container>
-        </ThemeProvider>
-    );
+	const clientSupabase = new VirtualOSBrowserCore();
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const dataForm = new FormData(event.currentTarget);
+		const { data, error } = await supabase.auth.signUp({
+			email: dataForm.get("email"),
+			password: dataForm.get("password"),
+		});
+		console.log(data);
+	};
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Sign in
+					</Typography>
+					<Box
+						component="form"
+						onSubmit={handleSubmit}
+						noValidate
+						sx={{ mt: 1 }}
+					>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="Email Address"
+							name="email"
+							autoComplete="email"
+							autoFocus
+						/>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Password"
+							type="password"
+							id="password"
+							autoComplete="current-password"
+						/>
+						<FormControlLabel
+							control={
+								<Checkbox value="remember" color="primary" />
+							}
+							label="Remember me"
+						/>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+							onClick={() => {
+								// router.push("/");
+							}}
+						>
+							Sign In
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link href="#" variant="body2">
+									Forgot password?
+								</Link>
+							</Grid>
+							<Grid item>
+								{/* <Link   */}
+								<NextLink href={AppRoutes.SIGN_UP}>
+									{"Don't have an account? Sign Up"}
+								</NextLink>
+								{/* </Link> */}
+							</Grid>
+						</Grid>
+						<Stack></Stack>
+						<Grid
+							container
+							justifyContent={"center"}
+							alignItems="center"
+						>
+							<Grid item xs="auto">
+								<Typography variant="h5">
+									Or sign in with:
+								</Typography>{" "}
+							</Grid>
+							<Grid item xs={"auto"}>
+								<IconButton
+									onClick={async () => {
+										await clientSupabase.LoginWithGoogle();
+									}}
+								>
+									<GoogleIcon
+										sx={{ fontSize: 30 }}
+										color="primary"
+									/>
+								</IconButton>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+				<Copyright sx={{ mt: 8, mb: 4 }} />
+			</Container>
+		</ThemeProvider>
+	);
 }
--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -14,137 +14,167 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { default as NextLink } from "next/link";
 import { AppRoutes } from "../constants/appRoutes";
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
+const validationSchema = Yup.object().shape({
+	email: Yup.string()
+		.required('Email is required')
+		.email('Email is invalid'),
+	password: Yup.string()
+		.required('Password is required')
+		.min(6, 'Password must be at least 6 characters')
+		.max(40, 'Password must not exceed 40 characters'),
+	confirmPassword: Yup.string()
+		.required('Confirm Password is required')
+		.oneOf([Yup.ref('password'), null], 'Confirm Password does not match'),
+})
 function Copyright(props: any) {
-    return (
-        <Typography
-            variant="body2"
-            color="text.secondary"
-            align="center"
-            {...props}
-        >
-            {"Copyright © "}
-            <Link color="inherit" href="https://mui.com/">
-                ThinkMay
-            </Link>{" "}
-            {new Date().getFullYear()}
-            {"."}
-        </Typography>
-    );
+	return (
+		<Typography
+			variant="body2"
+			color="text.secondary"
+			align="center"
+			{...props}
+		>
+			{"Copyright © "}
+			<Link color="inherit" href="https://mui.com/">
+				ThinkMay
+			</Link>{" "}
+			{new Date().getFullYear()}
+			{"."}
+		</Typography>
+	);
 }
 
 const theme = createTheme();
 
 export default function SignUp() {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get("email"),
-            password: data.get("password"),
-        });
-    };
+	const {
+		register,
+		control,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
+		resolver: yupResolver(validationSchema)
+	});
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign up
-                    </Typography>
-                    <Box
-                        component="form"
-                        noValidate
-                        onSubmit={handleSubmit}
-                        sx={{ mt: 3 }}
-                    >
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    autoComplete="given-name"
-                                    name="firstName"
-                                    required
-                                    fullWidth
-                                    id="firstName"
-                                    label="First Name"
-                                    autoFocus
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="lastName"
-                                    label="Last Name"
-                                    name="lastName"
-                                    autoComplete="family-name"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="new-password"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox
-                                            value="allowExtraEmails"
-                                            color="primary"
-                                        />
-                                    }
-                                    label="I want to receive inspiration, marketing promotions and updates via email."
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign Up
-                        </Button>
-                        <Grid container justifyContent="flex-end">
-                            <Grid item>
-                                {/* <Link variant="body2"> */}
-                                    <NextLink href={AppRoutes.SIGN_IN}>
-                                        Already have an account? Sign in
-                                    </NextLink>
-                                {/* </Link> */}
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-                <Copyright sx={{ mt: 5 }} />
-            </Container>
-        </ThemeProvider>
-    );
+	//const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+	//	event.preventDefault();
+	//	const data = new FormData(event.currentTarget);
+	//	console.log({
+	//		email: data.get("email"),
+	//		password: data.get("password"),
+	//	});
+	//};
+	const onSubmit = (data) => {
+		console.log(data);
+	}
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Sign up
+					</Typography>
+					<Box
+						component="form"
+						noValidate
+						onSubmit={handleSubmit(e => onSubmit(e))}
+						sx={{ mt: 3 }}
+					>
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									id="email"
+									label="Email Address"
+									name="email"
+									autoComplete="email"
+									{...register('email')}
+									error={errors.email ? true : false}
+								/>
+								<Typography variant="inherit" color="textSecondary">
+									{errors.email?.message}
+								</Typography>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									name="password"
+									label="Password"
+									type="password"
+									id="password"
+									{...register('password')}
+									error={errors.password ? true : false}
+								/>
+								<Typography variant="inherit" color="textSecondary">
+									{errors.password?.message}
+								</Typography>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									name="confirmPassword"
+									label="Confirm Password"
+									type="password"
+									id="confirmPassword"
+									autoComplete="confirmPassword"
+									{...register('confirmPassword')}
+									error={errors.confirmPassword ? true : false}
+								/>
+								<Typography variant="inherit" color="textSecondary">
+									{errors.confirmPassword?.message}
+								</Typography>
+							</Grid>
+							<Grid item xs={12}>
+								<FormControlLabel
+									control={
+										<Checkbox
+											value="allowExtraEmails"
+											color="primary"
+										/>
+									}
+									label="I want to receive inspiration, marketing promotions and updates via email."
+								/>
+							</Grid>
+						</Grid>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Sign Up
+						</Button>
+						<Grid container justifyContent="flex-end">
+							<Grid item>
+								{/* <Link variant="body2"> */}
+								<NextLink href={AppRoutes.SIGN_IN}>
+									Already have an account? Sign in
+								</NextLink>
+								{/* </Link> */}
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+				<Copyright sx={{ mt: 5 }} />
+			</Container>
+		</ThemeProvider>
+	);
 }
