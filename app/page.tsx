7dd55e75e40916d30b2039b026413ef06763be52
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-05-16 13:19:38 +0700

    disable preload video

--- app/page.tsx
+++ app/page.tsx
@@ -144,7 +144,7 @@ export default function Home () {
         <Body>
             <RemoteVideo
                 ref={remoteVideo}
-                src={platform == 'desktop' ? video_desktop : video_desktop}
+                // src={platform == 'desktop' ? video_desktop : video_desktop}
                 autoPlay
                 muted
                 playsInline
