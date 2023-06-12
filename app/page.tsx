482fffbac94bd0256d82d9c9c13c91dc454782a3
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-06-12 14:47:32 +0000

    add frameloss indicator

--- app/page.tsx
+++ app/page.tsx
@@ -101,6 +101,8 @@ export default function Home () {
                         })
                     }
                     setMetrics(dat)
+                case 'FRAME_LOSS':
+                    console.log("frame loss occur")
                     break;
                 default:
                     break;
