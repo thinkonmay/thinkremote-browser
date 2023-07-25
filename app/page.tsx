6a3cfc9ff9e887b59c9f4e5e0689e180e8b80004
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-07-25 15:39:30 +0700

    Revert "add feature block close tab"
    This reverts commit b0d35ebacf0325c521b87e0811bb2e3b0b7b2aea.

--- app/page.tsx
+++ app/page.tsx
@@ -72,15 +72,7 @@ export default function Home () {
         warehouse.WarehouseLoggingSession()
         if (!await core.Authenticated() && user_ref == undefined) 
                 await core.LoginWithGoogle()
-
-        window.addEventListener('beforeunload', (e) => {
-            e.returnValue = ''
-            e.preventDefault()
-        });
-        
-        window.addEventListener('unload', () => {
-            warehouse.WarehousePush('close')
-        });
+            
         if(ref == null) 
             return
 
