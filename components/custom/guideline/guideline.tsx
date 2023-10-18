49955f1f0f2ce2d411874bcd128a50add68d330e
carosaome <sieunhankiet@gmail.com>
2023-10-18 20:28:48 +0700

    add en language

--- components/custom/guideline/guideline.tsx
+++ components/custom/guideline/guideline.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import styled from 'styled-components';
 import SportsEsportsOutlinedIcon from '@mui/icons-material/SportsEsportsOutlined';
 import MouseOutlinedIcon from '@mui/icons-material/MouseOutlined';
@@ -9,7 +9,7 @@ import { Fullscreen, LockReset, } from "@mui/icons-material";
 import VideoSettingsOutlinedIcon from '@mui/icons-material/VideoSettingsOutlined';
 import KeyboardIcon from '@mui/icons-material/Keyboard';
 import SettingsIcon from '@mui/icons-material/Settings';
-
+import LanguageIcon from '@mui/icons-material/Language';
 const Container = styled.div`
 	width: 350px;
     height: fit-content;
@@ -25,6 +25,12 @@ const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: 8px;
+	font-size:14px;
+	@media (min-width: 1024px) {
+		width: 560px;
+		padding: 45px 32px 40px 32px;
+
+  }
 	
 `
 
@@ -36,6 +42,10 @@ const Content = styled.div`
 	gap: 8px;
 
 	align-items: center;
+	@media (min-width: 1024px) {
+		/*width: 500px;*/
+		font-size: 20px;
+  }
 `
 const ContainerIcon = styled.div`
 	border-radius: 50%;
@@ -43,23 +53,34 @@ const ContainerIcon = styled.div`
 	width: 24px;
 	min-width: 24px;
 	height: 24px;
-	font-size: 14px;
+	font-size: 16px;
 
 	display: flex;
 	justify-content: center;
 	align-items: center;
 
+	@media (min-width: 1024px) {
+		width: 48px;
+		min-width: 48px;
+		height: 48px;
+		font-size: 24px;
+  }
 `
 
 const Text = styled.span`
-	font-size: 14px;
-
+	font-size: 16px;
+	@media (min-width: 1024px) {
+		font-size: 20px;
+  }
 `
 const Title = styled.h3`
 	font-size: 18px;
-	font-bold: 600;
 	color: #222222;
 	text-align: center;
+	@media (min-width: 1024px) {
+		font-size: 24px;
+		margin-bottom: 24px;
+  }
 `
 const ContainerButton = styled.div`
 	display: flex;
@@ -91,48 +112,103 @@ const ButtonGotit = styled(ResetButton)`
 	color: black;
 	
 `
+const LanguageSwitch = styled.div`
+	&::after {
+        display: none;
+      }
+
+      select {
+        background: rgba(255, 255, 255, 0.8);
+        border: 1px solid rgb(238, 236, 235);
+        border-bottom: 1px solid rgb(212, 211, 210);
+        color: rgb(var(--txt_clr-rgb));
+        height: 32px;
+        padding-left: 8px;
+        border-radius: 6px;
+      }	
+`
 const buttonsMobile = [
 	{
-		icon: <VideoSettingsOutlinedIcon fontSize='inherit'/>,
-		name: "Tăng giảm chất lượng hình ảnh",
+		icon: <VideoSettingsOutlinedIcon fontSize='inherit' />,
+		name: {
+			vn: "Tăng giảm chất lượng hình ảnh",
+			en: "Change picture quality"
+		},
 
 	},
 	{
-		icon: <KeyboardIcon fontSize='inherit'/>,
-		name: "Mở bàn phím ảo",
+		icon: <KeyboardIcon fontSize='inherit' />,
+		name: {
+			vn: "Mở bàn phím ảo",
+			en: "Open virtual keyboard"
+		}
 
 	},
 	{
-		icon: <LockReset fontSize='inherit'/>,
-		name: "Reset khi không lên hình hoặc mất tiếng",
+		icon: <LockReset fontSize='inherit' />,
+		name: {
+			vn: "Reset khi không lên hình hoặc mất tiếng",
+			en: "Reset when no picture or audio"
+		}
 	},
 	{
-		icon: <SettingsIcon fontSize='inherit'/>,
-		name: "Thay đổi kích thước, vị trí của gamepad ảo.",
+		icon: <SettingsIcon fontSize='inherit' />,
+		name: {
+			vn: "Thay đổi kích thước, vị trí của gamepad ảo.",
+			en: "Change size and position for gamepad"
+		}
 	}
 ]
 const buttonsDesktop = [
 	{
-		icon: <LockReset fontSize='inherit'/>,
-		name: "Reset khi không lên hình hoặc mất tiếng",
+		icon: <LockReset fontSize='inherit' />,
+		name: {
+			vn: "Reset khi không lên hình hoặc mất tiếng",
+			en: "Reset when no picture or audio"
+		}
 	},
 	{
-		icon: <VideoSettingsOutlinedIcon fontSize='inherit'/>,
-		name: "Tăng giảm chất lượng hình ảnh",
+		icon: <VideoSettingsOutlinedIcon fontSize='inherit' />,
+		name: {
+			vn: "Tăng giảm chất lượng hình ảnh",
+			en: "Change picture quality"
+		}
 
 	},
 	{
-		icon: <SportsEsportsOutlinedIcon fontSize='inherit'/>,
-		name: "Tạo QR, quét để dùng điện thoại thay thế gamepad",
+		icon: <SportsEsportsOutlinedIcon fontSize='inherit' />,
+		name: {
+			vn: "Tạo QR, quét để dùng điện thoại thay thế gamepad",
+			en: "Create QR, to transform mobile into gamepad. Scan it"
+		}
 
 	},
 	{
-		icon: <Fullscreen fontSize='inherit'/>,
-		name: "Lưu ý: bắt buộc fullscreen khi chơi game",
+		icon: <Fullscreen fontSize='inherit' />,
+		name: {
+			vn: "Lưu ý: bắt buộc fullscreen khi chơi game",
+			en: "Notice: Playing requires fullscreen for best experience~"
+		}
 	}
 ]
+
+const title = {
+	vn: "CHÚ Ý: Mở game trong ổ D, không lưu file ở ổ C",
+	en: "NOTICE: Game's in disk D, mustn't save file in disk C"
+}
+const dontShow = {
+	vn: "Không hiện lại",
+	en: "Don't show it again~"
+}
+type Language = 'vn' | 'en'
 function GuideLine({ isModalOpen, closeModal, platform }) {
 	const [buttons, setButtons] = React.useState(buttonsDesktop)
+	const [language, setLanguage] = React.useState<Language>('en')
+
+	useLayoutEffect(()=>{
+		const languageLocal = localStorage.getItem('language') as Language
+		if(languageLocal) setLanguage(languageLocal)
+	},[])
 	const handleDontShow = () => {
 		localStorage.setItem('isGuideModalLocal', 'false')
 		closeModal()
@@ -142,34 +218,49 @@ function GuideLine({ isModalOpen, closeModal, platform }) {
 		closeModal()
 	}
 
-	React.useEffect(()=>{
-		if(platform == 'mobile'){
+	React.useEffect(() => {
+		if (platform == 'mobile') {
 			setButtons(buttonsMobile)
 			return
 		}
 		setButtons(buttonsDesktop)
 
-	},[platform])
+	}, [platform])
 	return (
 		<Modal open={isModalOpen}>
 			<Container>
-				<Title>CHÚ Ý: Mở game trong ổ D, không lưu file ở ổ C</Title>
+				<Title>{title[language]}</Title>
+				<div style={{display: 'flex', gap: 8, alignItems:'center', marginLeft: 'auto'}}>	
+					<LanguageIcon fontSize='large'/>
+					<LanguageSwitch>
+						<select
+							value={language}
+							onChange={(e) => {
+								setLanguage(e.target.value as Language);
+								localStorage.setItem('language', e.target.value)
+							}}
+						>
+							<option value="en">English</option>
+							<option value="vn">Vietnamese</option>
+						</select>
+					</LanguageSwitch>
+				</div>
 				{
-					buttons.map((btn,index) => (
+					buttons.map((btn, index) => (
 						<Content
 							key={index}
 						>
 							<ContainerIcon>
 								{btn.icon}
 							</ContainerIcon>
-							<Text>{btn.name}</Text>
+							<Text>{btn.name[language]}</Text>
 						</Content>
 					))
 				}
-				
 
-				<ContainerButton style={{marginTop: 14}}>
-					<ButtonDontShow onClick={handleDontShow}>Không hiện lại</ButtonDontShow>
+
+				<ContainerButton style={{ marginTop: 14 }}>
+					<ButtonDontShow onClick={handleDontShow}>{dontShow[language]}</ButtonDontShow>
 					<ButtonGotit onClick={handleGotit}>Oke!</ButtonGotit>
 				</ContainerButton>
 			</Container>
